e8f2f98dabfecb76ee02f83d3959e773a1f05a06
sawa-mon <sawada.6284@gmail.com>
2020-10-09 22:11:04 +0900

    Material-UIにてスタイルあて

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({ // material-ui の見た目をカスタマイズする
-  root: {
-    
-  },
-}));
+const useStyles = makeStyles(() => ( // material-ui の見た目をカスタマイズする
+    createStyles({
+      "button": {
+        borderColor: '#FFB549',
+        color: '#FFB549',
+        fontWeight: 600,
+        marginBottom: '8px',
+        "&:hover": {
+          backgroundColor: '#FFB549',
+          color: '#fff'
+        }
+      }
+    })
+));
 
 const Answer = (contentOf) => {
-  // const classes = useStyles();
+  const classes = useStyles();
 
   return(
-    <Button variant="contained" color="primary" onClick={() => contentOf.select(contentOf.content, contentOf.nextId)} >
+    <Button
+      className={classes.button}
+      variant="outlined" onClick={() => contentOf.select(contentOf.content, contentOf.nextId)}
+    >
       {contentOf.content}
     </Button>
   )
